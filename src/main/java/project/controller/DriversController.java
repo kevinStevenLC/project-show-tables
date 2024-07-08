52f1fe338308523fdfd91b9bc4ddbe5b195cc4e3
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-08 10:40:18 -0500

    agregando controlador de la tabla drivers, y pruebas

--- src/main/java/project/controller/DriversController.java
+++ src/main/java/project/controller/DriversController.java
@@ -1,6 +1,7 @@
 package project.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -9,34 +10,73 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import project.model.DriverResult;
+import project.respository.DriverResultRepository;
 
 public class DriversController implements Initializable {
 
+	// Table
+	@FXML
+	private TableView<DriverResult> tableDrivers;
+
+	// Columns
+
+	@FXML
+	private TableColumn<DriverResult, String> ColumnDriverName;
+
+	@FXML
+	private TableColumn<DriverResult, Integer> ColumnRank;
+
+	@FXML
+	private TableColumn<DriverResult, Integer> ColumnTotalPoints;
+
+	@FXML
+	private TableColumn<DriverResult, Integer> ColumnWins;
+
+	// Selector
+
+	@FXML
+	private ComboBox<Integer> selectionYear;
+
+	// Variables
+	private int selectedYear;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		// Agregando Opcioens
+		for (int year = 1950; year <= 2019; year++) {
+			selectionYear.getItems().add(year);
+		}
+
+		cargarDrivers(2004);
+
+		// valor default
+		selectionYear.setValue(2004);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@FXML
+	private void cargarDrivers(int year) {
+		this.ColumnDriverName.setCellValueFactory(new PropertyValueFactory("driverName"));
+		this.ColumnRank.setCellValueFactory(new PropertyValueFactory("rank"));
+		this.ColumnTotalPoints.setCellValueFactory(new PropertyValueFactory("totalPoints"));
+		this.ColumnWins.setCellValueFactory(new PropertyValueFactory("wins"));
+
+		DriverResultRepository drv = new DriverResultRepository();
+		List<DriverResult> ls_d = drv.getResultByYear(year);
+		for (DriverResult rs : ls_d) {
+			tableDrivers.getItems().add(rs);
+		}
 
 	}
 
 	@FXML
-	private TableColumn<?, ?> ColumnDriverName;
-
-	@FXML
-	private TableColumn<?, ?> ColumnRank;
-
-	@FXML
-	private TableColumn<?, ?> ColumnTotalPoints;
-
-	@FXML
-	private TableColumn<?, ?> ColumnWins;
-
-	@FXML
-	private ComboBox<?> selectionYear;
-
-	@FXML
-	private TableView<?> tableDrivers;
-
-	@FXML
 	void selectionMethod(ActionEvent event) {
-
+		selectedYear = selectionYear.getValue();
+		System.out.println("Year selected: " + selectedYear);
+		// Limpiar la tabla antes de cargar nuevos datos
+		tableDrivers.getItems().clear();
+		cargarDrivers(selectedYear);
 	}
 }
---
+++ src/main/java/project/respository/DriverResultRepository.java
@@ -0,0 +1,86 @@
+package project.respository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+import project.model.DbConexion;
+import project.model.DriverResult;
+
+public class DriverResultRepository {
+
+	public List<DriverResult> getResultByYear(int year) {
+
+		List<DriverResult> results = new ArrayList<DriverResult>();
+
+		try {
+			// Establecer la conexion
+			DbConexion db_conexion = new DbConexion();
+			Connection conn = db_conexion.EstablecerConexion("test_database", "postgres", "593321", "postgresql");
+
+			// Ejecutar la consulta
+			String sql = "SELECT\n"
+					+ "    r.year,\n"
+					+ "    d.forename,\n"
+					+ "    d.surname,\n"
+					+ "    COUNT(CASE WHEN res.position = 1 THEN 1 END) AS wins,\n"
+					+ "    SUM(res.points) AS total_points,\n"
+					+ "    RANK() OVER (PARTITION BY r.year ORDER BY SUM(res.points) DESC) AS season_rank\n"
+					+ "FROM\n"
+					+ "    results res\n"
+					+ "JOIN\n"
+					+ "    races r ON res.race_id = r.race_id\n"
+					+ "JOIN\n"
+					+ "    drivers d ON res.driver_id = d.driver_id\n"
+					+ "\n"
+					+ "WHERE r.year = ? \n"
+					+ "GROUP BY\n"
+					+ "    r.year, d.driver_id, d.forename, d.surname\n"
+					+ "ORDER BY\n"
+					+ "    r.year, season_rank;";
+
+			// Crear una sentencia
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, year);
+
+			ResultSet rs = statement.executeQuery();
+
+			// Procesar los resultados
+			while (rs.next()) {
+				String forename = rs.getString("forename");
+				String surname = rs.getString("surname");
+				int wins = rs.getInt("wins");
+				int total_points = rs.getInt("total_points");
+				int season_rank = rs.getInt("season_rank");
+
+				DriverResult result = new DriverResult(forename + " " + surname, wins, total_points, season_rank);
+				results.add(result);
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return results;
+
+	}
+
+	public static void main(String[] args) {
+		DriverResultRepository drv = new DriverResultRepository();
+		List<DriverResult> ls_d = drv.getResultByYear(2009);
+
+		// Imprimir encabezado de la tabla
+		System.out.printf("%-20s %-10s %-10s %8s%n", "DriverName", "Wins", "Total Points", "Rank");
+
+		// Imprimir cada registro con formato
+		for (DriverResult rs : ls_d) {
+			System.out.printf("%-20s %-10d %-15d  %-10d%n",
+					rs.getDriverName(),
+					rs.getWins(),
+					rs.getTotalPoints(),
+					rs.getRank());
+		}
+	}
+}
