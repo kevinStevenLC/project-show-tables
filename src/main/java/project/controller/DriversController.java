7b620ed0a875c19a9671a37d14ef4c9e1c8c139f
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-08 10:47:39 -0500

    Modificando DriversController

--- src/main/java/project/controller/DriversController.java
+++ src/main/java/project/controller/DriversController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import project.model.DriverResult;
-import project.respository.DriverResultRepository;
+import project.repository.DriverResultRepository;
 
 public class DriversController implements Initializable {
 
